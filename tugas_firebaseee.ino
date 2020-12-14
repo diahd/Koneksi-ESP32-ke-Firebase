8fffa533a09b21bf5c9fa8719443219b832d9f56
diahd <diahdwiyanti93@gmail.com>
2020-12-14 20:48:52 +0700

    first commit

---
+++ tugas_firebaseee.ino
@@ -0,0 +1,70 @@
+#include <IOXhop_FirebaseESP32.h>
+#include <IOXhop_FirebaseStream.h>
+#include <WiFi.h>
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// Set these to run example.
+#define FIREBASE_HOST "FIREBASE URL"
+#define FIREBASE_AUTH "FIREBASE API/TOKEN"
+#define WIFI_SSID "WIFI NAME"
+#define WIFI_PASSWORD "WIFI PASSWORD"
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+  dht.begin();
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+int n = 0;
+
+void loop() {
+  //Baca sensor suhu dan kelembaban
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+    if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  
+  // set value
+  Firebase.setFloat("suhu", t);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  Firebase.setFloat("kelembaban", h);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  Serial.println ("Data Suhu dan Kelembaban Sukses Terkirim");
+  delay(1000);
+}
